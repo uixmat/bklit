297f0be652851707161380a6c61340644caf92c2
Matt <mattsince87@gmail.com>
2025-05-17 21:27:01 +0300

    updates

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: ".next", // Explicitly set the distDir
+  // reactStrictMode: true, // Good default
+};
+
+export default nextConfig;
