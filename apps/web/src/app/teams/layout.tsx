005963bbc6b1d406fd75ece964fc597059f1de76
Matt <mattsince87@gmail.com>
2025-08-03 13:17:33 +0300

    feat/create-team-ux

---
+++ apps/web/src/app/teams/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex flex-col gap-4 justify-center items-center h-screen">
+			{children}
+		</div>
+	);
+}
