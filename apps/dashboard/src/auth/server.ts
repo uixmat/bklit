bfc95a8070c2b85c0496899912a84557ad047a84
Dimitar Mitov <dimitarmitoff@gmail.com>
2025-08-16 08:56:05 +0300

    fix auth

--- apps/dashboard/src/auth/server.ts
+++ apps/dashboard/src/auth/server.ts
@@ -1,12 +1,12 @@
 import { initAuth } from "@bklit/auth";
-import { envs } from "@bklit/utils";
+import { dashboardUrl } from "@bklit/utils/envs";
 import { headers } from "next/headers";
 import { cache } from "react";
 import { env } from "../env";
 
 export const auth = initAuth({
-  baseUrl: envs.dashboardUrl(),
-  productionUrl: envs.dashboardUrl(),
+  baseUrl: dashboardUrl(),
+  productionUrl: dashboardUrl(),
   secret: env.AUTH_SECRET,
   githubClientId: env.AUTH_GITHUB_ID,
   githubClientSecret: env.AUTH_GITHUB_SECRET,
